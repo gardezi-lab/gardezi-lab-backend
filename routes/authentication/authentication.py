641208a35617cdb33ce99ad0a89f31b26f32ad54
Muhammad Mazhar <contactmazhar88@gmail.com>
2025-10-01 10:53:34 +0500

    add status code in login api

--- routes/authentication/authentication.py
+++ routes/authentication/authentication.py
@@ -42,7 +42,7 @@ def login_user():
         token = jwt.encode({"some": "payload"}, "secret", algorithm="HS256")
 
         return jsonify({
-            "status": 20033,
+            "status": 2003,
             "message": "Login successful",
             "token": token,
             "user": user
